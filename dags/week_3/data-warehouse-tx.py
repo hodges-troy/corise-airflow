0e04bfa513e80eaf75fdb7a2062534fad9e926ca
hodges-troy <tjhodges12@gmail.com>
2023-03-04 16:49:42 -0800

    Update data-warehouse-tx.py

--- dags/week_3/data-warehouse-tx.py
+++ dags/week_3/data-warehouse-tx.py
@@ -1,67 +1,23 @@
 from datetime import datetime
-
-import pandas as pd
 from typing import List
 
-from airflow.operators.empty import EmptyOperator
+import pandas as pd
 from airflow.decorators import dag, task, task_group
+from airflow.operators.empty import EmptyOperator
+
+from common.week_3.config import DATA_TYPES, normalized_columns
+
 
 # PROJECT_ID = # Modify HERE
 # DESTINATION_BUCKET = # Modify HERE
 # BQ_DATASET_NAME = # Modify HERE
-
-DATA_TYPES = ["generation", "weather"] 
-
-
-normalized_columns = {
-    "generation": {
-        "time": "time",
-        "columns": 
-            [   
-                "total_load_actual",
-                "price_day_ahead",
-                "price_actual",
-                "generation_fossil_hard_coal",
-                "generation_fossil_gas",
-                "generation_fossil_brown_coal_lignite",
-                "generation_fossil_oil",
-                "generation_other_renewable",
-                "generation_waste",
-                "generation_biomass",
-                "generation_other",
-                "generation_solar",
-                "generation_hydro_water_reservoir",
-                "generation_nuclear",
-                "generation_hydro_run_of_river_and_poundage",
-                "generation_wind_onshore",
-                "generation_hydro_pumped_storage_consumption"
-
-            ]
-        },
-    "weather": {
-        "time": "dt_iso",
-        "columns": 
-            [
-                "city_name",
-                "temp",
-                "pressure",
-                "humidity",
-                "wind_speed",
-                "wind_deg",
-                "rain_1h",
-                "rain_3h",
-                "snow_3h",
-                "clouds_all",
-            ]
-        }
-    }
 
 
 @dag(
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    ) 
+)
 def data_warehouse_transform_dag():
     """
     ### Data Warehouse Transform DAG
@@ -73,21 +29,18 @@ def data_warehouse_transform_dag():
         5. Builds a joined view on top of the normalized views, joined on time
     """
 
-
     @task
     def extract() -> List[pd.DataFrame]:
         """
         #### Extract task
         A simple task that loads each file in the zipped file into a dataframe,
         building a list of dataframes that is returned
-
-
         """
         from zipfile import ZipFile
         filename = "/usr/local/airflow/dags/data/energy-consumption-generation-prices-and-weather.zip"
-        dfs = [pd.read_csv(ZipFile(filename).open(i)) for i in ZipFile(filename).namelist()]
+        dfs = [pd.read_csv(ZipFile(filename).open(i)) for i in
+               ZipFile(filename).namelist()]
         return dfs
-
 
     @task
     def load(unzip_result: List[pd.DataFrame]):
@@ -98,60 +51,63 @@ def data_warehouse_transform_dag():
         """
 
         from airflow.providers.google.cloud.hooks.gcs import GCSHook
-        
-        client = GCSHook().get_conn()       
+
+        client = GCSHook().get_conn()
         bucket = client.get_bucket(DESTINATION_BUCKET)
 
         for index, df in enumerate(unzip_result):
             df.columns = df.columns.str.replace(" ", "_")
             df.columns = df.columns.str.replace("/", "_")
             df.columns = df.columns.str.replace("-", "_")
-            bucket.blob(f"week-3/{DATA_TYPES[index]}.parquet").upload_from_string(df.to_parquet(), "text/parquet")
+            bucket.blob(
+                f"week-3/{DATA_TYPES[index]}.parquet").upload_from_string(
+                df.to_parquet(), "text/parquet")
             print(df.dtypes)
 
-    @task
+    @task_group
     def create_bigquery_dataset():
-        from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+        from airflow.providers.google.cloud.operators.bigquery import \
+            BigQueryCreateEmptyDatasetOperator
         EmptyOperator(task_id='placeholder')
         # TODO Modify here to create a BigQueryDataset if one does not already exist
         # This is where your tables and views will be created
-    
 
     @task_group
     def create_external_tables():
-        from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+        from airflow.providers.google.cloud.operators.bigquery import \
+            BigQueryCreateExternalTableOperator
         EmptyOperator(task_id='placeholder')
 
         # TODO Modify here to produce two external tables, one for each data type, referencing the data stored in GCS
 
         # When using the BigQueryCreateExternalTableOperator, it's suggested you use the table_resource
         # field to specify DDL configuration parameters. If you don't, then you will see an error
-        # related to the built table_resource specifying csvOptions even though the desired format is 
+        # related to the built table_resource specifying csvOptions even though the desired format is
         # PARQUET.
 
-
-    def produce_select_statement(timestamp_column: str, columns: List[str]) -> str:
-        # TODO Modify here to produce a select statement by casting 'timestamp_column' to 
+    def produce_select_statement(timestamp_column: str,
+                                 columns: List[str]) -> str:
+        # TODO Modify here to produce a select statement by casting 'timestamp_column' to
         # TIMESTAMP type, and selecting all of the columns in 'columns'
         pass
 
     @task_group
     def produce_normalized_views():
-        from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+        from airflow.providers.google.cloud.operators.bigquery import \
+            BigQueryCreateEmptyTableOperator
         # TODO Modify here to produce views for each of the datasources, capturing only the essential
-        # columns specified in normalized_columns. A key step at this stage is to convert the relevant 
+        # columns specified in normalized_columns. A key step at this stage is to convert the relevant
         # columns in each datasource from string to time. The utility function 'produce_select_statement'
-        # accepts the timestamp column, and essential columns for each of the datatypes and build a 
+        # accepts the timestamp column, and essential columns for each of the datatypes and build a
         # select statement ptogrammatically, which can then be passed to the Airflow Operators.
         EmptyOperator(task_id='placeholder')
 
-
-    @task
+    @task_group
     def produce_joined_view():
-        from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+        from airflow.providers.google.cloud.operators.bigquery import \
+            BigQueryCreateEmptyTableOperator
         # TODO Modify here to produce a view that joins the two normalized views on time
         EmptyOperator(task_id='placeholder')
-
 
     unzip_task = extract()
     load_task = load(unzip_task)
