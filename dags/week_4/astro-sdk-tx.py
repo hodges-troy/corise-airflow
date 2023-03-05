7bcc83469cfca2d45370ff3d2fbce6ea8f5dab6c
hodges-troy <tjhodges12@gmail.com>
2023-03-05 12:42:03 -0800

    fill in logic for load file

--- dags/week_4/astro-sdk-tx.py
+++ dags/week_4/astro-sdk-tx.py
@@ -6,9 +6,11 @@ import astro.sql as aql
 from astro.files import File
 from astro.table import Metadata, Table
 
+from common.week_3.config import DATA_TYPES
 
 
-BQ_DATASET_NAME = "timeseries_energy"
+BQ_DATASET_NAME = "energy_prediction"
+GCP_CONNECTION_ID = "google_cloud_default"
 
 
 time_columns = {
@@ -17,8 +19,8 @@ time_columns = {
 }
 
 filepaths = {
-    # "generation": fill in location of data from week 3
-    # "weather": " fill in location of data from week 3
+    "generation": "gs://corise-airflow-tjh/week-3/generation.parquet",
+    "weather": "gs://corise-airflow-tjh/week-3/weather.parquet"
 }
 
 
@@ -29,8 +31,8 @@ def extract_nonzero_columns(input_df: pd.DataFrame) -> pd.DataFrame:
     calling fillna(0) and only selecting columns that have any non-zero elements
     Fill null values with 0 for filtering and extract only columns that have
     """
-
     # TODO Modify here
+    # keep columns that have at least one non-zero value and fill in null values with 0
     pass
 
 
@@ -40,11 +42,10 @@ def convert_timestamp_columns(input_table: Table, data_type: str):
     Return a SQL statement that selects the input table elements, 
     casting the time column specified in 'time_columns' to TIMESTAMP
     """
-    
     # TODO Modify here
+    # parquet file has time column as string. Cast to TIMESTAMP.
     pass
 
-    
 
 @aql.transform
 def join_tables(generation_table: Table, weather_table: Table):  # skipcq: PYL-W0613
@@ -73,10 +74,20 @@ with DAG(
     sense for how to work with Astro SDK. For some examples of how it can be used, check out 
     # https://github.com/astronomer/astro-sdk/blob/main/python-sdk/example_dags/example_google_bigquery_gcs_load_and_save.py
     """
-
     # TODO Modify here
-   
 
+    for data_type in DATA_TYPES:
+        # Load parquet files from GCS into BigQuery using Table object
+        task_1 = aql.load_file(
+            task_id=f"load_{data_type}",
+            input_file=File(path=filepaths[data_type]),
+            output_table=Table(
+                name=f"{data_type}_raw",
+                metadata=Metadata(schema=BQ_DATASET_NAME),
+                gcp_conn_id=GCP_CONNECTION_ID,
+            )
+        )
+
+        # Extract nonzero columns from that table
     # Cleans up all temporary tables produced by the SDK
     aql.cleanup()
-
